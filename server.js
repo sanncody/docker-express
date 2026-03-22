61fbcbd136fd6ec34f09993d5a3eaac4364ebc5d
sanncody <sandeepwadhawan2001@gmail.com>
2026-03-23 02:04:16 +0530

    Added legacy-watch flag with nodemon as it is not updating in docker env and changed response message in server file to get reflected on running server using docker container

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const app = express();
 
 app.get('/', (req, res) => {
     res.status(200).json({
-        message: "Welcome to Express.js in Docker"
+        message: "Testing: Volume mounting in docker with express image"
     })
 });
 
